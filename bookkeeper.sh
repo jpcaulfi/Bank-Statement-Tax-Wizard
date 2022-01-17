17b202deadca397f8e9ad21a231582aa69c80266
jpcaulfi <joecaulfield29@yahoo.com>
2022-01-16 20:35:09 -0500

    Added comments and TODOs for 2nd draft

--- bookkeeper.sh
+++ bookkeeper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Enter the repository with cd and run this command:
+# Enter this repository with cd and run this command:
 #    sudo bash bookkeeper.sh
 
 # Enter your database information here:
@@ -22,6 +22,8 @@ sleep 2
 
 # Reset DB
 python3 reset_db.py $DATABASE_STRING $SCHEMA_NAME $DATABASE_USERNAME $DATABASE_PASSWORD
+
+# Launch message
 echo -e "\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 echo "------------------------------------------------------------"
 echo "Launching Bank Statement Tax Wizard"
@@ -30,6 +32,7 @@ echo -e "\n\n\n\n\n\n\n\n\n"
 sleep 3
 
 # Process PDF files into transaction log
+## Make a list of all PDF files in the imports folder
 start_time=$(date +%M)
 _pdf_files="$(find ./imports/ -name "*.pdf")"
 if [[ $_pdf_files == "" ]];
@@ -43,36 +46,59 @@ rm -rf results/
 rm -rf temp/
 mkdir results
 mkdir temp
+
+## Iterate over each PDF file
 for f in $_pdf_files
 do
   echo "Processing PDF file $f"
+
+  ## Extract the bank name and the account number (last 4 digits) from the PDF file
   BANK_NAME="$(pdfgrep -m 1 -o '[a-z]*'"\.com" "$f" | sed 's/.com//g')"
   echo "-bank-name: $BANK_NAME" >> ./temp/import.txt
   ACCOUNT_NUM="$(pdfgrep -m 1 "Account number" "$f" | grep -o '[0-9]\+$')"
   echo "-account-num: $ACCOUNT_NUM" >> ./temp/import.txt
+
+  ## Determine the number of statements present and the line numbers they begin at
   NUMBER_OF_STATEMENTS="$(pdfgrep "" $f | grep -i 'beginning balance on ' | wc -l)"
   LINE_NUMBERS=(`pdfgrep "" $f | grep -i -n 'beginning balance on ' | cut -d : -f 1`)
+  END_OF_FILE=$(pdfgrep "" $f | wc -l)
+#  declare -i END_OF_FILE=$(pdfgrep "" $f | wc -l)
+
+  ## Iterate over each statement
   for i in $(seq 1 $NUMBER_OF_STATEMENTS)
   do
+
+    # TODO: Fix block end identifier (withdrawals not always present)
+    ### Identify the size (in lines) of the current statement
     if [[ ${LINE_NUMBERS[($i)]} == "" ]];
     then
-      declare -i END_OF_FILE=$(pdfgrep "" $f | wc -l)
       declare -i BLOCK_SIZE=$END_OF_FILE-${LINE_NUMBERS[($i-1)]}+1
     else
       declare -i BLOCK_SIZE=${LINE_NUMBERS[($i)]}-${LINE_NUMBERS[($i-1)]}
     fi
     declare -i GREP_INCREMENT=$BLOCK_SIZE-1
+
+    ### Extract the statement's start and end dates
     echo "-start: $(pdfgrep "" $f | grep -m $i -A $GREP_INCREMENT -i 'beginning balance on ' | tail -$BLOCK_SIZE | grep -E -i -o 'beginning balance on [A-Za-z]{1,9}+[ ]*+[0-9]{1,2}+[,]+[ ]*+[0-9]{1,4}' | sed 's/beginning balance on //gi')" >> ./temp/import.txt
     echo "-end: $(pdfgrep "" $f | grep -m $i -A $GREP_INCREMENT -i 'beginning balance on ' | tail -$BLOCK_SIZE | grep -E -i -o 'ending balance on [A-Za-z]{1,9}+[ ]*+[0-9]{1,2}+[,]+[ ]*+[0-9]{1,4}' | sed 's/ending balance on //gi')" >> ./temp/import.txt
+
+    # TODO: Fix transaction section identifying (sections not always present)
+    # TODO: Add functionality to extract fees section as well
+    ### Identify the size of the each transactions section
     declare -i TRANSACTIONS_SECTION_END="$(pdfgrep "" $f | grep -m $i -A $BLOCK_SIZE -i 'beginning balance on ' | tail -$BLOCK_SIZE | grep -n -i 'total withdrawals' | cut -d : -f 1)"
     TRANSACTIONS_LINE_NUMBERS=(`pdfgrep "" $f | grep -m $i -A $BLOCK_SIZE -i 'beginning balance on ' | tail -$BLOCK_SIZE | grep -n -i 'date[ ]*description[ ]*amount' | cut -d : -f 1`)
     declare -i DEPOSITS_SECTION_SIZE=${TRANSACTIONS_LINE_NUMBERS[1]}-${TRANSACTIONS_LINE_NUMBERS[0]}-1
     declare -i WITHDRAWALS_SECTION_SIZE=$TRANSACTIONS_SECTION_END-${TRANSACTIONS_LINE_NUMBERS[1]}-1
+
+    ### Extract all deposits
     echo "-deposits:" >> ./temp/import.txt
     pdfgrep "" $f | grep -m $i -A $GREP_INCREMENT -i 'beginning balance on ' | tail -$BLOCK_SIZE | grep -m 1 -A $DEPOSITS_SECTION_SIZE -i 'date[ ]*description[ ]*amount' | grep '[0-9]*\.[0-9]*' | grep '[0-9]*\/[0-9]*' | grep -E '[0-9]{1,2}\/[0-9].*[0-9]{1,3}\.[0-9]{1,2}' >> ./temp/import.txt
+
+    ### Extract all withdrawals
     echo "-withdrawals:" >> ./temp/import.txt
     pdfgrep "" $f | grep -m $i -A $GREP_INCREMENT -i 'beginning balance on ' | tail -$BLOCK_SIZE | grep -m 2 -A $WITHDRAWALS_SECTION_SIZE -i 'date[ ]*description[ ]*amount' | grep '[0-9]*\.[0-9]*' | grep '[0-9]*\/[0-9]*' | grep -E '[0-9]{1,2}\/[0-9].*[0-9]{1,3}\.[0-9]{1,2}' >> ./temp/import.txt
     echo "-stop:" >> ./temp/import.txt
+
   done
   echo " "
   echo "Processed PDF file $f successfully"
@@ -95,11 +121,11 @@ python3 import.py $DATABASE_STRING $SCHEMA_NAME $DATABASE_USERNAME $DATABASE_PAS
 echo "All transaction data imported into database successfully"
 
 # Sort bank accounts
-python3 sort_accounts.py $DATABASE_STRING $SCHEMA_NAME $DATABASE_USERNAME $DATABASE_PASSWORD
+#python3 sort_accounts.py $DATABASE_STRING $SCHEMA_NAME $DATABASE_USERNAME $DATABASE_PASSWORD
 
 # Sort transactions
 #python3 reset_transactions.py $DATABASE_STRING $SCHEMA_NAME $DATABASE_USERNAME $DATABASE_PASSWORD
-python3 sort_transactions.py $DATABASE_STRING $SCHEMA_NAME $DATABASE_USERNAME $DATABASE_PASSWORD
+#python3 sort_transactions.py $DATABASE_STRING $SCHEMA_NAME $DATABASE_USERNAME $DATABASE_PASSWORD
 
 # Generate results
-python3 results.py $DATABASE_STRING $SCHEMA_NAME $DATABASE_USERNAME $DATABASE_PASSWORD "$COMPANY"
+#python3 results.py $DATABASE_STRING $SCHEMA_NAME $DATABASE_USERNAME $DATABASE_PASSWORD "$COMPANY"
